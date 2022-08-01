af2be6ee8b2102d70b4afcdea87c4beec2c8e3d9
Bordin Intarama <tam@Bordins-MacBook-Pro.local>
2022-08-01 20:16:28 +0700

    feat: 🎸 Add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,92 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import LogoImage from '../../assets/images/logo.svg'
 import styles from './index.module.scss'
+
+const Hamburger = (): React.ReactElement => {
+  return (
+    <div id="btn-hamburger" className={styles.btn__hamburger}>
+      <div className={styles.icon__wrapper}>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+      </div>
+    </div>
+  )
+}
+
+const Logo = (): React.ReactElement => {
+  return (
+    <div className={styles.nav__logo}>
+      <Link to="/" target="">
+        <img
+          className={styles.brand__logo}
+          src={LogoImage}
+          alt="Disney+&nbsp;Hotstar"
+        />
+      </Link>
+    </div>
+  )
+}
+
+const Navigation = (): React.ReactElement => {
+  const navigationItems = [
+    {
+      to: '/tv',
+      label: 'Series'
+    },
+    {
+      to: '/movie',
+      label: 'Movies'
+    },
+    {
+      to: '/disneyplus',
+      label: 'Disney+'
+    },
+    {
+      to: '/local',
+      label: 'Asian'
+    },
+    {
+      to: '',
+      label: '',
+      isKid: true
+    }
+  ]
+
+  return (
+    <div className={styles.navigations} role="navigation">
+      <ul>
+        {navigationItems.map((item) => (
+          <li key={item.label}>
+            <div className={styles.dropdown__container}>
+              <Link className={styles.nav__link} to={item.to} target="">
+                <div
+                  className={
+                    item.isKid ? `${styles.iconClass} ${styles.kids}` : ''
+                  }
+                >
+                  {item.label}
+                </div>
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 const Header = (): React.ReactElement => {
   return (
     <div className={styles.header__container}>
       <div className={styles.static__container}>
         <div className={`${styles.header} ${styles.fixed__header}`}>
-          <div className={styles.nav__container}>Nav Container</div>
+          <div className={styles.nav__container}>
+            <Hamburger />
+            <Logo />
+            <Navigation />
+          </div>
         </div>
       </div>
     </div>
